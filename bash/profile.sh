f07f97892a8fd5297f2ea27eea6cd48d41f85882
Michael Wyman <michael@mwyman.com>
2014-04-02 13:52:18 -0700

    Update profile

--- bash/profile.sh
+++ bash/profile.sh
@@ -5,17 +5,27 @@
 CURRENT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
 # Include our aliases
-source "${CURRENT_DIR}/alias.sh"
+if [ -f "${CURRENT_DIR}/alias.sh" ]; then
+  source "${CURRENT_DIR}/alias.sh"
+fi
 
 # Bring in our color definitions
-source "${CURRENT_DIR}/colors.sh"
+if [ -f "${CURRENT_DIR}/colors.sh" ]; then
+  source "${CURRENT_DIR}/colors.sh"
+fi
 
 # Setup git TAB-completion and the shell prompt hooks
-source "${CURRENT_DIR}/git-completion.sh"
-source "${CURRENT_DIR}/git-prompt.sh"
+if [ -f "${CURRENT_DIR}/git-completion.sh" && -f "${CURRENT_DIR}/git-prompt.sh" ]; then
+  source "${CURRENT_DIR}/git-completion.sh"
+  source "${CURRENT_DIR}/git-prompt.sh"
+fi
 
 # Make sure our TAB-completion in BASH is case-insensitive
-INPUTRC="${CURRENT_DIR}/input.inputrc"
+if [ -f "${CURRENT_DIR}/input.inputrc" ]; then
+  INPUTRC="${CURRENT_DIR}/input.inputrc"
+fi
 
+# Setup the shell prompt:
+# uname (git branch) dirname$ 
 PS1="\[${Blue}\]\u\[${BRed}\]\$(__git_ps1) \[${Purple}\]\W\[${Green}\]\$\[${Color_Off}\] "
 
