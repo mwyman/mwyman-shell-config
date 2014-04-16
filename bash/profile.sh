29027d115f69267d9a1e0869988bc1c50e63cfe3
Michael Wyman <michael@mwyman.com>
2014-04-16 10:54:30 -0700

    Pulling git-prompt/git-completion scripts from Xcode, if available

--- bash/profile.sh
+++ bash/profile.sh
@@ -15,12 +15,40 @@ if [ -f "${CURRENT_DIR}/colors.sh" ]; then
 fi
 
 # Setup git TAB-completion and the shell prompt hooks
-if [ -f "${CURRENT_DIR}/git-completion.sh" ]; then
-  source "${CURRENT_DIR}/git-completion.sh"
+# Attempt to use Xcode's built-in completion (may be newer)
+if [ -n "$(which xcode-select)" ]; then
+  XCODE_DIR=$(xcode-select --print-path)
+
+  GIT_COMPLETION_SH="${XCODE_DIR}/usr/share/git-core/git-completion.bash"
+  GIT_PROMPT_SH="${XCODE_DIR}/usr/share/git-core/git-prompt.sh"
+else
+  GIT_COMPLETION_SH="${CURRENT_DIR}/git-completion.sh"
+  GIT_PROMPT_SH="${CURRENT_DIR}/git-prompt.sh"
 fi
 
-if [ -f "${CURRENT_DIR}/git-prompt.sh" ]; then
-  source "${CURRENT_DIR}/git-prompt.sh"
+if [ -f "${GIT_COMPLETION_SH}" ]; then
+  source "${GIT_COMPLETION_SH}"
+fi
+
+if [ -f "${GIT_PROMPT_SH}" ]; then
+  source "${GIT_PROMPT_SH}"
+
+  # Show unsaged ('*') and staged ('+') file status in the prompt
+  export GIT_PS1_SHOWDIRTYSTATE=1
+
+  # Show '$' if the stash is non-empty
+  export GIT_PS1_SHOWSTASHSTATE=1
+
+  # Show untracked (%) file status in the prompt
+  export GIT_PS1_SHOWUNTRACKEDFILES=1
+
+  # Show the difference between HEAD and its upstream, with
+  # a '<' indicating you're behind, '>' indicating you're ahead.
+  # A '<>' indicates divergence and '=' indicating no difference.
+  #export GIT_PS1_SHOWUPSTREAM="auto"
+
+  # Show color hints
+  #export GIT_PS1_SHOWCOLORHINTS=1
 fi
 
 # Make sure our TAB-completion in BASH is case-insensitive
