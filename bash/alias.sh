43d6d49feebcaa6b6ab8016c64b63f184688d93e
Michael Wyman <michael@mwyman.com>
2014-05-13 14:34:04 -0700

    Fix bad dircolors behavior on Mac

--- bash/alias.sh
+++ bash/alias.sh
@@ -1,5 +1,11 @@
 # Bash aliases
 
-alias ls="ls -F --color=auto"
-alias grep="grep --color=auto"
+# Enable color support of ls and grep
+if [ -x /usr/bin/dircolors ]; then
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  alias ls="ls -F --color=auto"
+  alias grep="grep --color=auto"
+else
+  alias ls="ls -F"
+fi
 
