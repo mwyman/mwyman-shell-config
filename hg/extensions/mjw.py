d3706d7a0623018360c99129c14ce53fc59b4384
Michael Wyman <michael@mwyman.com>
2018-07-23 10:37:50 -0700

    Fix mjw.py for latest Mercurial.

--- hg/extensions/mjw.py
+++ hg/extensions/mjw.py
@@ -7,11 +7,12 @@ import os
 
 from mercurial import commands
 from mercurial import cmdutil
+from mercurial import registrar
 from mercurial import error
 from mercurial import ui as uimod
 
 cmdtable = {}
-command = cmdutil.command(cmdtable)
+command = registrar.command(cmdtable)
 
 CONFIG_SECTION='mjw'
 
